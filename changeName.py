ade0b2b45a0c4a454c121c1393635d95ad1e5b5e
Jack Toubes <jack2bs@me.com>
2023-12-11 09:33:59 -0500

    changes

--- changeName.py
+++ changeName.py
@@ -10,6 +10,7 @@ def create_namespace_pattern(namespaces):
     # Join all namespace names with '|' to create a pattern that matches any of them
     namespace_names = '|'.join(map(re.escape, namespaces.keys()))
     # Pattern to match 'namespace::'
+    namespace_names = namespace_names.strip("|")
     pattern = r'\b({})::'.format(namespace_names)
     return pattern
 
@@ -26,34 +27,42 @@ def replace_var_names(c_code, namespaces):
     # Regular expression for namespace usage
     namespace_usage_pattern = create_namespace_pattern(namespaces)
 
-
     for i, line in enumerate(lines):
         
         # Replace 'namespace::' with 'namespace__'
         match = re.search(namespace_usage_pattern, line)
-        if match:
+        while match:
+            print(line, i)
             match = namespace_map[match.group(1)]
             line = re.sub(namespace_usage_pattern, match+'__', line) 
+            match = re.search(namespace_usage_pattern, line)
+
             
         namespace_match = re.match(r'\s*namespace\s+((\w|\$)+)\s*\{', line) # Assuming 'namespace XYZ {' format
         if namespace_match:
             # Extract namespace name from the regex match
             namespace_name = namespace_match.group(1)
-            namespace_vars = set(namespaces.get(namespace_name, []))
-            current_namespace = Namespace(namespace_name, namespace_vars)
             if namespace_stack:
                 # add all outer namespace names to this inner one before appending
                 for outer_name in namespace_stack:
-                    current_namespace.name = outer_name.name + '__' + current_namespace.name
+                    namespace_name = outer_name.name + '__' + namespace_name
+            namespace_vars = set(namespaces.get(namespace_name, []))
+            current_namespace = Namespace(namespace_name, namespace_vars)
+
             namespace_stack.append(current_namespace)
             namespace_map[namespace_name] = current_namespace.name
             continue
 
         elif namespace_stack:
             used = []
+
+            
+
+
             # Replace variable names with "TEST" using regex for whole word matching
             for namespace in reversed(namespace_stack):
                 for var in namespace.vars:
+                    
                     if var not in used:
                         pattern = r'\b' + re.escape(var) + r'\b'  # Word boundary regex
                         used.append(var)    # should it be 'used.append(pattern)' ??
